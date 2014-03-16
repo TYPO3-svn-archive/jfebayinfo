552bed859b94963f7e971b5dd091c6298b9ae00c
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-16 22:27:04 +0000

    First Version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jfebayinfo/trunk@84313 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "jfebayinfo".
-#
-# Auto generated 17-11-2012 22:12
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jfebayinfo".
+ *
+ * Auto generated 16-03-2014 23:25
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'EBAY Info',
@@ -17,29 +17,33 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Juergen Furrer',
 	'author_email' => 'juergen.furrer@gmail.com',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 1,
-	'createDirs' => 'uploads/tx_jfebayinfo',
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
+			'cms' => '',
+			'php' => '5.0.0-0.0.0',
+			'typo3' => '4.3.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"2b7b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"a7ca";s:14:"ext_tables.php";s:4:"f1a6";s:13:"locallang.xml";s:4:"8023";s:16:"locallang_db.xml";s:4:"124f";s:19:"doc/wizard_form.dat";s:4:"0d6f";s:20:"doc/wizard_form.html";s:4:"1523";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:31:"pi1/class.tx_jfebayinfo_pi1.php";s:4:"bb4e";s:39:"pi1/class.tx_jfebayinfo_pi1_wizicon.php";s:4:"99cd";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"1abf";s:29:"static/ebayinfo/constants.txt";s:4:"d41d";s:25:"static/ebayinfo/setup.txt";s:4:"d41d";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"2b7b";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"a7ca";s:14:"ext_tables.php";s:4:"1b73";s:13:"locallang.xml";s:4:"8023";s:16:"locallang_db.xml";s:4:"124f";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"0d6f";s:20:"doc/wizard_form.html";s:4:"1523";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:31:"pi1/class.tx_jfebayinfo_pi1.php";s:4:"d126";s:39:"pi1/class.tx_jfebayinfo_pi1_wizicon.php";s:4:"99cd";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"1abf";s:29:"static/ebayinfo/constants.txt";s:4:"b26e";s:25:"static/ebayinfo/setup.txt";s:4:"3979";}',
+	'suggests' => array(
+	),
 );
 
 ?>
